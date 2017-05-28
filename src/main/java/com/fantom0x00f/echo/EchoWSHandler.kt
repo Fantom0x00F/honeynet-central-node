ea3749ffd254d4fc9bb0b521829dc2c88e9f0f62
fantom0x00F <hominem19@gmail.com>
2017-05-28 15:32:00 +0400

    add connection verification

--- src/main/java/com/fantom0x00f/echo/EchoWSHandler.kt
+++ src/main/java/com/fantom0x00f/echo/EchoWSHandler.kt
@@ -3,6 +3,7 @@ package com.fantom0x00f.echo
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.socket.CloseStatus
 import org.springframework.web.socket.TextMessage
@@ -19,12 +20,29 @@ open class EchoWSHandler : TextWebSocketHandler() {
     @Autowired
     lateinit var echoService: EchoService
 
+    @Value("\${agent.secret.onconnection}")
+    private lateinit var expectedSecret: String
+
+    @Value("\${agent.secret.response}")
+    private lateinit var responseSecret: String
+
     override fun handleTextMessage(session: WebSocketSession?, message: TextMessage?) {
         val messagePayload = message?.payload ?: "error"
+        val attributes = session?.attributes!!
+        if (!attributes.containsKey("Verified")) {
+            if (messagePayload != expectedSecret) {
+                session.close()
+                logger.error("Bad secret")
+                return
+            }
+            attributes["Verified"] = "true"
+            session.sendMessage(TextMessage(responseSecret))
+            return
+        }
         logger.info("Received message $messagePayload")
         val response = echoService.getResponse(messagePayload)
         logger.info("Prepared response $messagePayload")
-        session?.sendMessage(TextMessage(response))
+        session.sendMessage(TextMessage(response))
     }
 
     override fun afterConnectionEstablished(session: WebSocketSession?) {
