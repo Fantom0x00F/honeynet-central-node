3a6ce179b28e8058415611d61f35ba1414f811f8
Andrey Efimov <andrey.efimov@magenta-technology.com>
2017-11-29 21:12:08 +0400

    fix mistakes in test configurator

--- src/test/java/com/fantom0x00f/genetic/TestConfigurationBuilder.kt
+++ src/test/java/com/fantom0x00f/genetic/TestConfigurationBuilder.kt
@@ -4,7 +4,7 @@ import com.fantom0x00f.entity.NodeGroup
 import com.fantom0x00f.network.impls.NetworkServiceImpl
 import java.util.*
 
-class TestConfigurationBuilder(val countForSearch: Int) {
+class TestConfigurationBuilder(private val countForSearch: Int) {
 
     fun getTestNetwork(groupsCount: Int): List<NodeGroup> {
         val networkService = NetworkServiceImpl()
@@ -15,8 +15,10 @@ class TestConfigurationBuilder(val countForSearch: Int) {
             result.add(newGroup)
             val linkedGroups = random.nextInt(countForSearch)
 
-            if (result.isNotEmpty()) {
-                networkService.linkNodes(newGroup, *((0..linkedGroups).map { result[random.nextInt(result.size)].id }.toIntArray()))
+            if (result.size > 1) {
+                val adjGroups = (0..linkedGroups).map { result[random.nextInt(result.size - 1)].id }.toIntArray()
+                assert(!adjGroups.contains(newGroup.id))
+                networkService.linkNodes(newGroup, *adjGroups)
             }
         }
         return result
